2740837ac2a931036ae1688f3ecbf29813c56165
Eric Schall <eric@humboldtgeoscience.com>
2021-11-09 20:54:48 -0600

    fix

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+const qcard = document.querySelectorAll(".q-card")
+const arrows = document.querySelectorAll(".arrow")
+
+for (let i = 0; i < qcard.length; i++) {
+    qcard[i].addEventListener("click", function() {
+        this.classList.toggle("active")
+    })
+}
